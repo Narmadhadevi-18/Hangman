c75941f21a10f705737dc2cfd4e6cd7f395afaa4
Narmadhadevi-18 <115984768+Narmadhadevi-18@users.noreply.github.com>
2025-09-19 10:56:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,115 @@
+import random 
+
+word_list=['apple','microsoft','google','nvdia']
+
+# Hangman stages list
+hangman_stages = [
+    """
+       ------
+       |    |
+       |
+       |
+       |
+       |
+    --------
+    """,
+    """
+       ------
+       |    |
+       |    O
+       |
+       |
+       |
+    --------
+    """,
+    """
+       ------
+       |    |
+       |    O
+       |    |
+       |
+       |
+    --------
+    """,
+    """
+       ------
+       |    |
+       |    O
+       |   /|
+       |
+       |
+    --------
+    """,
+    """
+       ------
+       |    |
+       |    O
+       |   /|/
+       |
+       |
+    --------
+    """,
+    """
+       ------
+       |    |
+       |    O
+       |   /|/
+       |   /
+       |
+    --------
+    """,
+    """
+       ------
+       |    |
+       |    O
+       |   /|/
+       |   / /
+       |
+    --------
+    """
+]
+
+
+chosen_word=random.choice(word_list)
+print(chosen_word)
+print(hangman_stages[len(hangman_stages)-1])
+
+placeholder=""
+for p in range(len(chosen_word)):
+    placeholder+=" _"
+print(placeholder)
+
+# Game Starts
+Game_over=False
+correct_letters=[]
+chance=5
+while not Game_over:
+    print(f"YOU HAVE {chance} lives ")
+    
+    guess=input("Guess the letter: ").lower()
+    if guess in correct_letters:
+        print("You've already guessed")
+    display=""
+    for letter in chosen_word:
+        if letter == guess:
+            display+=letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display+=letter
+        else:
+            display+='_'+" " 
+                
+    print(f"guess again :{display}")  
+
+    if guess not in chosen_word:
+        chance-=1
+        print(f"You have guessed{guess}, that's not in the word. You lose a life.")
+        if chance==0:
+            Game_over=True
+            print(f"IT WAS {chosen_word} YOU LOSE")
+    if "_" not in display:
+        Game_over=True
+        print("YOU WON")   
+
+    print(hangman_stages[chance])         
+
